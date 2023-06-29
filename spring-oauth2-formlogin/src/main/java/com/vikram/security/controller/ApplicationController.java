0932af2c86dfb82555f9b4f99730aa60e7cbaaec
vikram.bindal <vikram.bindal@entelect.co.za>
2023-06-29 07:46:05 +0200

    property fix for secret and getting oauth2 principal name

--- spring-oauth2-formlogin/src/main/java/com/vikram/security/controller/ApplicationController.java
+++ spring-oauth2-formlogin/src/main/java/com/vikram/security/controller/ApplicationController.java
@@ -1,5 +1,6 @@
 package com.vikram.security.controller;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,10 @@ public class ApplicationController {
 
     @GetMapping(value = "hello")
     public String sayHello(Principal principal) {
-        return "Hello " + principal.getName();
+        String name = principal.getName();
+        if (principal instanceof OAuth2AuthenticationToken) {
+            name = ((OAuth2AuthenticationToken) principal).getPrincipal().getAttribute("name");
+        }
+        return "Hello " + name;
     }
 }
